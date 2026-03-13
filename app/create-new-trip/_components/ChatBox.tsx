5d3e779bb8e2e85d928d0feb2a221f05ce348bbd
dhruv284 <agarwaldhruv284@gmail.com>
2026-03-13 23:41:10 +0530

    setting up AI

--- app/create-new-trip/_components/ChatBox.tsx
+++ app/create-new-trip/_components/ChatBox.tsx
@@ -2,33 +2,66 @@
 import { Button } from '@/components/ui/button'
 import { Textarea } from '@/components/ui/textarea'
 import { Send } from 'lucide-react'
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
+type Message={
+    role: string,
+    content: string
+}
 function ChatBox() {
-    const onsend=()=>{
+
+    const [messages,setMessages]=useState<Message[]>([])
+    const [userInput,setUserInput] = useState<string>()   
+    const onsend=async ()=>{
+        if (!userInput?.trim()){
+            return 
+
+        }
+        setUserInput('')
+        const newMsg:Message={
+            role: 'user',
+            content: userInput
+        }
+        setMessages((prev:Message[])=>[...prev,newMsg]);
+        const result= await axios.post('/api/aimodel',{
+            messages: [...messages,newMsg]
+        })
+        setMessages((prev: Message[])=>[...prev,{
+            role: 'assistant',
+            content: result?.data?.resp
+        }])
+        console.log(result.data)
 
     }
+
   return (
     
-    <div>
+    <div className='h-[85vh] flex flex-col'>
         <section className='flex-1 overflow-y-auto p-4'>
-            <div className='flex justify-end mt-2'>
-                <div className='max-w-lg bg-primary text-white px-4 py-2 rounded-lg'>
-                    User Msg
-
-                </div>
-            </div>
-            <div className='flex justify-start mt-2'>
-                <div className='max-w-lg bg-gray-100 text-black px-4 py-2 rounded-lg'>
-                    AI Agent Msg
-
-                </div>
-            </div>
+            {messages.map((msg: Message, index) => (
+                msg.role === "user" ? (
+                    <div key={index} className='flex justify-end mt-2'>
+                    <div className='max-w-lg bg-primary text-white px-4 py-2 rounded-lg'>
+                        {msg.content}
+                    </div>
+                    </div>
+                ) : (
+                    <div key={index} className='flex justify-start mt-2'>
+                    <div className='max-w-lg bg-gray-100 text-black px-4 py-2 rounded-lg'>
+                        {msg.content}
+                    </div>
+                    </div>
+                )
+            ))}
+            
 
         </section>
         <section>
             <div className='border rounded-2xl p-4 relative'>
-                <Textarea placeholder='Create a trip from Paris to New York' className='w-full h-28 bg-transparent border-none focus-visible:ring-0 shadow-none resize-none'></Textarea>
+                <Textarea placeholder='Create a trip from Paris to New York' className='w-full h-28 bg-transparent border-none focus-visible:ring-0 shadow-none resize-none'
+                onChange={(event)=>setUserInput(event.target.value)}
+                value={userInput}></Textarea>
                 <Button size={'icon'} className='absolute bottom-6 right-6' onClick={()=>onsend()}>
                     <Send className='h-4 w-4'/>
                 </Button>
